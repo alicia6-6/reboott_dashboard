c2fb83076234491799c2ef34ae68c899172a6b8f
Alicia <hotkoco@gmail.com>
2026-05-20 12:48:26 +0900

    기트허브 페이지 배포 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 테일윈드 플러그인을 여기에 쏙 넣어줍니다!
+    tailwindcss(),
   ],
+  base: '/reboott_dashboard/', // 기트허브 저장소
 })
